6f79f6a954fc8aed0db9bbfb2d902f635f763b80
spaceballtwo <samfwilliams4@icloud.com>
2024-12-04 20:48:01 -0800

    1

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ let mySprite4 = sprites.create(img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    `, SpriteKind.Player)
+    `, SpriteKind.Enemy)
 let mysprite3 = sprites.create(img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
@@ -74,15 +74,21 @@ let mySprite = sprites.create(img`
     . . a a a a a a a a a a a a . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
+mySprite.setPosition(scene.screenWidth() / 2, scene.screenHeight() - 12)
 mySprite2.setVelocity(0, 50)
 mysprite3.setVelocity(0, 50)
 mySprite4.setVelocity(0, 50)
-mySprite.setPosition(scene.screenWidth() / 2, scene.screenHeight() - 12)
-mySprite2.setPosition(scene.screenWidth() - 10, scene.screenHeight() - 150)
-info.changeScoreBy(1)
-pause(3500)
-mySprite2.setPosition(scene.screenWidth() / 2, scene.screenHeight() - 150)
-mysprite3.setPosition(scene.screenWidth() - 20, scene.screenHeight() - 150)
-mySprite4.setPosition(scene.screenWidth() - 100, scene.screenHeight() - 150)
-pause(3500)
-info.changeScoreBy(1)
+forever(function () {
+    mySprite2.setPosition(scene.screenWidth() - 10, scene.screenHeight() - 150)
+    mysprite3.setPosition(scene.screenWidth() - 50, scene.screenHeight() - 100)
+    mySprite4.setPosition(scene.screenWidth() - 100, scene.screenHeight() - 150)
+    info.changeScoreBy(1)
+    pause(3500)
+    mySprite2.setPosition(scene.screenWidth() / 2, scene.screenHeight() - 150)
+    mysprite3.setPosition(scene.screenWidth() - 20, scene.screenHeight() - 150)
+    pause(3500)
+    info.changeScoreBy(1)
+    mySprite2.ay += 2
+    mysprite3.ay += 2
+    mySprite4.ay += 2
+})
